9b8905fa77722b0158421a4b23cfbdf0fb9cb162
nate <nathan.whippo@gmail.com>
2024-09-23 14:39:51 -0400

    initial commit

---
+++ Cpu.cpp
@@ -0,0 +1,63 @@
+#include <cstdint>
+#include <string>
+#include <unordered_map>
+function readMemory(Int[32] ROM, Int[32] RAM, Int[16] Registers) {
+
+
+
+//max len of instructional set it 3 bytes
+int pc = 0x100;
+int cycle = 0;
+
+
+
+
+
+while(true){
+
+switch(instruction){
+    int opcode = (instruction & 0xF000);
+
+    //1 byte instructional sets
+    case (opcode == 0x00){
+        cycles+=
+    }
+
+    case(opcode == 0x76){
+        break;
+    }
+
+    case(opcode == 0x10){
+        break;
+    }
+    
+    //2bytes instructional sets
+
+}
+    
+instruction = instruction
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
+}
---
+++ opCodes.cpp
@@ -0,0 +1,23 @@
+#include <Cpu.h>
+
+
+
+
+//these are indices in our register global. 
+function movlnton(uint16_t regA, uint16_t regB){
+
+
+}
+
+function movlntonn(){
+
+}
+
+function movlnnton(){
+
+}
+
+function movlnntonn(){
+
+}
+
