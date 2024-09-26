de077a9e08e4cbc9da8f66847a1ad429a837ae3d
nate <nawhippo>
2024-09-25 22:17:38 -0400

    testing ssh for desktop

--- opCodes.cpp
+++ opCodes.cpp
@@ -1,29 +1,45 @@
 #include <Cpu.h>
-
-
-
+#include <stdint.h>
+#include <Globals.h>
+#include <Registers.h>
 
 //these are indices in our register global. 
-function movlnton(uint16_t regA, uint16_t regB){
+void movlnton(uint16_t regA, uint16_t regB){
 
 
 }
 
-function movlntonn(uint16_t regA, uint16_t regB){
+void movlntonn(uint16_t regA, uint16_t regB){
 
 }
 
-function movlnnton(uint16_t regA, uint16_t regB){
+void movlnnton(uint16_t regA, uint16_t regB){
 
 }
 
-function movlnntonn(uint16_t regA, uint16_t regB){
+void movlnntonn(uint16_t regA, uint16_t regB){
 
 }
 
-function movlntoHL(uint16_t regA, uint16_t regB){
+void movlntoHL(uint16_t regA, uint16_t regB){
 
 }
+
+
+
+//ADD Operations 
+//REG A is the destination
+
+//we have to handle over flow.
+
+//registers should be handled by index?
+void addnton(uint8_t regA, uint8_t regB){
+    regA = regA + regB;
+    return regA; 
+}
+
+
+
 
 
 
