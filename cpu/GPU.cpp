6825fc63e214727c7c34980a95612cf7194e9fbc
Nathan Whippo <93534375+nawhippo@users.noreply.github.com>
2025-08-07 16:35:07 -0400

    removed constances, becauase they have been added to the memory bus functions

--- cpu/GPU.cpp
+++ cpu/GPU.cpp
@@ -3,10 +3,6 @@
 #include <algorithm>
 #include <tile.h>
 class GPU {
-static const uint8_t VRAM_BEGIN = 0x8000;
-static const uint8_t VRAM_END = 0x9FFF;
-static const uint8_t VRAM_SIZE = VRAM_END - VRAM_BEGIN + 1;
-uint8_t vram[VRAM_SIZE];
 Tile tile_set[384];
-
+//vram should be a part of the memory bus 
 };
