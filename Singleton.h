ab593d0da5a16c65e7c9744ba60583ccf16e9951
Nathan Whippo <93534375+nawhippo@users.noreply.github.com>
2025-10-15 11:55:08 -0400

    added additional functionality for gpu

--- Singleton.h
+++ Singleton.h
@@ -19,9 +19,19 @@ class Singleton {
     uint8_t ioRegisters[0x80];     // FF00–FF7F
     uint8_t hram[0x7F];            // FF80–FFFE
     uint8_t interruptEnable;        // FFFF
+    uint8_t* lcdc = &ioRegisters[0x40];               //FF40 - addy bus is 16 bits and data bus is 8 bits
+    uint8_t* stat = &ioRegisters[0x41];              //  FF41
+    uint8_t* lyc = &ioRegisters[0x42];               //  FF42
+    uint8_t* ly = &ioRegisters[0x44];                //  FF44 current horizontal line
     uint8_t* ROMPTR;
     uint16_t programcounter;
     uint16_t* stackPtr = reinterpret_cast<uint16_t*>(0xFFFF);
+    uint8_t lcd_registers[0x10]; // FF40 - FF4F
+    bool IME = 0; //interrupt master enable flag
+    uint8_t* IE = &ioRegisters[0xFFFF]; // FFFF 0 - VBlank 1 - LCD STAT 2 - Timer 3 - Serial 4 - Joypad 5 - 7 NONE (if interrupt is enabled, then we check interrupt flags)
+    uint8_t* IF = &ioRegisters[0xFF0F]; // FF0F interrupt flag register (THIS ARE FOR SIGNALS, NOT NECESSARILY ENABLED)
+     
+
 
 
 
@@ -53,23 +63,21 @@ public:
     uint8_t* getIORegisters() { return ioRegisters; }
     uint8_t* getHRAM() { return hram; }
     uint8_t& getInterruptEnable() { return interruptEnable; }
+    uint8_t* getUnusable() { return unusable; }
+    uint8_t* getLCDC() { return lcdc; }
+    uint8_t* getSTAT() { return stat; }
+    uint8_t* getLYC() { return lyc; }
+    uint8_t* getLY() { return ly; }
+    uint8_t* getLCDRegisters() { return lcd_registers; }
+    bool getIME() const { return IME; }
+    uint8_t* getIE() { return IE; }
+    uint8_t* getIF() { return IF; }
     
-
+    //OAMS Start when - 
     void push(uint16_t value) {
     stackPtr--;
     *stackPtr = value;
     }
-
-void initRAM() {
-
-}
-
-
-
-void step(){
-    programcounter = getROM(programcounter);
-
-}
 
 //cannot init 
 private:
